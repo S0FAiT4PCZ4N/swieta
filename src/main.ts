b2db8d38fd98ce719cfdd1817432866d3846f775
Seweryn Kozik <sewkozik@gmail.com>
2023-12-24 14:39:01 +0100

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 let swietadiv = document.getElementById("swieta")
 let text = "Wesołych świąt 2023!";
 
-for(let i = 0; i < text.length; i++) {
-    let letter = text.charAt(i);
-    let temptext = swietadiv?.innerHTML;
-    setTimeout(() => {
+let idx = 0;
+
+setInterval(() => {
+    if (idx < text.length) {
+        let temptext = swietadiv?.innerHTML;
+        let letter = text.charAt(idx);
         swietadiv!.innerHTML = temptext + letter;
-    })
-}
+        idx++;
+    }
+}, 1e3)
 

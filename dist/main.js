dfa485cbc53be7663bd3944c1ec5a19c3fe5f2ee
Seweryn Kozik <sewkozik@gmail.com>
2023-12-24 14:46:59 +0100

    Fix infinite loop

--- dist/main.js
+++ dist/main.js
@@ -2,11 +2,14 @@
 let swietadiv = document.getElementById("swieta");
 let text = "Wesołych świąt 2023!";
 let idx = 0;
-setInterval(() => {
+let interval = setInterval(() => {
     if (idx < text.length) {
         let temptext = swietadiv === null || swietadiv === void 0 ? void 0 : swietadiv.innerHTML;
         let letter = text.charAt(idx);
         swietadiv.innerHTML = temptext + letter;
         idx++;
+    }
+    else {
+        clearInterval(interval);
     }
 }, 100);
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ let text = "Wesołych świąt 2023!";
 
 let idx = 0;
 
-setInterval(() => {
+let interval = setInterval(() => {
     if (idx < text.length) {
         let temptext = swietadiv?.innerHTML;
         let letter = text.charAt(idx);
         swietadiv!.innerHTML = temptext + letter;
         idx++;
+    } else {
+        clearInterval(interval);
     }
 }, 100)
 
